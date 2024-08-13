825631ba39568de9e5f6a9b1d6e42bbf218033d4
Zeeptech Gaming <ZeeptechGaming@hotmail.com>
2024-08-13 16:03:02 +0200

    Make clicksound, add money, make it work

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,11 @@ class Player {
     static coinPurse = 0;
     static coinsPerAttack = 1;
     static clickCount =0;
-    
+    static cost = 10;
 
     static cracking(){
         document.getElementById("CanClicker").addEventListener("click",this.attack);
+        document.getElementById("upgrade").addEventListener("click", this.upgrade);
     }
 
     
@@ -26,14 +27,31 @@ class Player {
             Player.clickCount = 0;
         }
 
-
+        
         document.getElementById("displayMoney").textContent = Player.coinPurse;
         console.log(`You get ${Player.coinsPerAttack} coins every attack `);
         console.log(`You have ${Player.coinPurse} coin(s) `);
         console.log(`You have ${Player.clickCount} Click(s)`);
     }
 
+    static upgrade(cost, newCost, upgrade){
+        
+        newCost = Player.cost * 2;
+        upgrade = Player.coinsPerAttack + 1;
 
+
+
+        if (Player.coinPurse >= Player.cost) {
+            Player.coinsPerAttack += upgrade;
+            Player.coinPurse -= Player.cost;
+
+            Player.cost += newCost;
+
+            document.getElementById("displayCost").textContent = Player.cost;
+            document.getElementById("displayMoney").textContent = Player.coinPurse;
+        }
+       
+    }
 
 }
 Player.cracking();
